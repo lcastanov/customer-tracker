b92227d2e5c0557ba58f6fb2a2db5d54aec1868d
Laura <lauracastanovilla@usf.edu>
2025-09-14 15:45:01 -0400

    first commit

---
+++ cc_3a.js
@@ -0,0 +1,20 @@
+// Coding Challenge 00
+
+let customers = [{
+    name: "Jane",
+    email: "jane.pearson@gmail.com",
+    purchases: 12,
+},
+{
+    name: "Horace",
+    email: "itshoracep@gmail.com",
+    purchases: 5,
+},
+{
+    name: "Zoe",
+    email: "zoey101@gmail.com",
+    purchases: 101,
+}]
+
+newCustomer = {name: "Lila", email: "lilamoss@gmail.com", purchases: 20}
+customers.push(newCustomer)
