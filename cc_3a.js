b096afffe9a78393fb99e4ff5a18a78a390388db
Laura <lauracastanovilla@usf.edu>
2025-09-14 20:43:26 -0400

    updated purchases into strings

--- cc_3a.js
+++ cc_3a.js
@@ -3,33 +3,41 @@
 let customers = [{
     name: "Jane",
     email: "jane.pearson@gmail.com",
-    purchases: 12,
+    purchases: "extension cord, water bottle, backpack",
 },
 {
     name: "Horace",
     email: "itshoracep@gmail.com",
-    purchases: 5,
+    purchases: "USB, case",
 },
 {
     name: "Zoe",
     email: "zoey101@gmail.com",
-    purchases: 101,
+    purchases: "notebook, pencil, eraser, markers",
 },
 {
     name: "Carson",
     email: "carsonshome172@gmail.com",
-    purchases: 2,
+    purchases: "brush, gel",
 }]
 
 console.log(customers);
+
+// Added new customer
 
 newCustomer = {name: "Lila", email: "lilamoss@gmail.com", purchases: 20}
 customers.push(newCustomer)
 
 console.log(customers);
 
+// Removed first customer
+
 customers.shift();
+
+// Updates Zoey's email
 
 customers[1].email = ("zoey101@pca.edu")
 
 console.log(customers);
+
+customers.push()
